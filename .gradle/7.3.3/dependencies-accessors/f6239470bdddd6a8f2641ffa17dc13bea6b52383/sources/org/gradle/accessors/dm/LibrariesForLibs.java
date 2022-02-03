c1a45cfc585fa523a93e484b928344d1aac7874d
neroweneed <neroweneed@gmail.com>
2022-02-02 23:20:16 -0500

    .gitignore is now working

---
+++ .gradle/7.3.3/dependencies-accessors/f6239470bdddd6a8f2641ffa17dc13bea6b52383/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,90 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+*/
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final KotlinxLibraryAccessors laccForKotlinxLibraryAccessors = new KotlinxLibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(providers, config);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers) {
+        super(config, providers);
+    }
+
+    /**
+     * Returns the group of libraries at kotlinx
+     */
+    public KotlinxLibraryAccessors getKotlinx() { return laccForKotlinxLibraryAccessors; }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() { return vaccForVersionAccessors; }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() { return baccForBundleAccessors; }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() { return paccForPluginAccessors; }
+
+    public static class KotlinxLibraryAccessors extends SubDependencyFactory {
+
+        public KotlinxLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for datetime (org.jetbrains.kotlinx:kotlinx-datetime)
+             * This dependency was declared in settings file 'settings.gradle.kts'
+             */
+            public Provider<MinimalExternalModuleDependency> getDatetime() { return create("kotlinx.datetime"); }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: kotlin (1.6.10)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in settings file 'settings.gradle.kts'
+             */
+            public Provider<String> getKotlin() { return getVersion("kotlin"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+}
