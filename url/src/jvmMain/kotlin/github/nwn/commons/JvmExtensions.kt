a5c414233b84041519d88d9c8e7bf8467269c6a6
neroweneed <neroweneed@gmail.com>
2022-02-17 22:53:46 -0500

    - Url.toFile and File.toUrl now replace all path characters with system path character.

--- url/src/jvmMain/kotlin/github/nwn/commons/JvmExtensions.kt
+++ url/src/jvmMain/kotlin/github/nwn/commons/JvmExtensions.kt
@@ -4,8 +4,12 @@ import java.io.File
 import java.net.URL
 
 fun Url.toFile(): File =
-    if (this.scheme == FILE_SCHEME_NAME) File(this.path.value.percentDecoded()) else throw IllegalArgumentException("Url scheme " +
-            "must" +
-            " be $FILE_SCHEME_NAME")
-fun Url.toJVM() : URL = URL(this.toString())
-fun File.toUrl() = Url("${FILE_SCHEME_NAME}:${this.path}")
+    if (this.scheme == FILE_SCHEME_NAME) File(
+        this.path.value.replace('/', File.pathSeparatorChar)
+            .percentDecoded()
+    ) else throw IllegalArgumentException(
+        "Url scheme must be $FILE_SCHEME_NAME"
+    )
+
+fun Url.toJVM(): URL = URL(this.toString())
+fun File.toUrl() = Url("${FILE_SCHEME_NAME}:${this.path.replace(File.pathSeparatorChar,'/')}")
